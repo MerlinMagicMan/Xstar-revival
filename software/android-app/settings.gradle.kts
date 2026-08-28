ab9bb123d2950e81c5ebd0214ff041fbc113c3a4
MerlinMagicMan <j.neihart@solvixlms.com>
2026-08-28 13:33:40 -0500

    Initialize offline Android dashboard project

---
+++ software/android-app/settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "xstar-revival-android"
+include(":app")
+include(":appCore")
+project(":appCore").projectDir = file("../app-core")
